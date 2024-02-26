106f6a10a4c8a9d46f8189aea3288d514ffe201f
AkshayViswanathan <akshayviswanathan@techversantinfotech.com>
2024-02-26 18:18:12 +0530

    updated

--- src/components/QuestionComp.vue
+++ src/components/QuestionComp.vue
@@ -1,36 +1,42 @@
 <template>
-    <div>
+                      <!-- loading state -->
+    <div v-if="loading">
+        <looping-rhombuses-spinner
+  :animation-duration="2500"
+  :rhombus-size="15"
+  color="#ff1d5e"
+/>
+    </div>
+
+                        <!-- question  -->
+                        <div v-else>
         questions
-        <div v-for="apiArray in data.results" :key="apiArray.question">
-    <ul>
-        <li> 
+        <div v-for="(apiArray, index) in data.results" :key="index">
             {{ apiArray.question }}
-        </li> 
-    </ul>
-    <ol>
-        <li>
+
+
+                    <div>
+                      <!-- Correct answer -->
+              <input type="radio" :id="'correct_' + index" :name="'question' + index" :value="apiArray.correct_answer" />
+              <label :for="'correct' + index">{{ apiArray.correct_answer }}</label>
           
-            <input type="radio" :value="apiArray.correct_answer"  >
-            {{ apiArray.correct_answer }}
-          
-        
-        <div v-for="incorrect in apiArray.incorrect_answers" :key="incorrect">
-          
-            <input type="radio" :value="incorrect">
-            {{ incorrect }}
-          
-        </div>
-    </li>
-      </ol>
+                  <!-- Incorrect answers -->
+                     <div v-for="(incorrect, i) in apiArray.incorrect_answers" :key="i">
+                    <input type="radio" :id="'incorrect_' + index + '_' + i" :name="'question' + index" :value="incorrect" />
+                    <label :for="'incorrect' + index + '_' + i">{{ incorrect }}</label>
+                 </div>
+
+</div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import { LoopingRhombusesSpinner } from 'epic-spinners'
 
  const data = ref([]);
- const loading = ref()
+ const loading = ref(true)
 
 
 const fetchdata = async()=>{
@@ -39,6 +45,7 @@ const fetchdata = async()=>{
 
       const sucessData = await response.json();
       data.value = sucessData;
+      loading.value=false;
       console.log(data.value);
   }
   catch(error){
